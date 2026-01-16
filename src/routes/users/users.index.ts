50a7e49a60e0e13f915990781b4adcdb3d487e65
wkflanders <wflan@seas.upenn.edu>
2026-01-16 10:02:25 -0500

    feat: init users routers

---
+++ src/routes/users/users.index.ts
@@ -0,0 +1,10 @@
+import * as handlers from "@/handlers/users/users.handlers";
+import createRouter from "@/lib/createRouter";
+import * as routes from "@/routes/users/users.routes";
+
+const router = createRouter()
+  .openapi(routes.list, handlers.list)
+  .openapi(routes.create, handlers.create)
+  .openapi(routes.getOne, handlers.getOne);
+
+export default router;
