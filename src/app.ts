5c0e08d05c0b1d1c2017934b15790ca67616d874
wkflanders <wflan@seas.upenn.edu>
2026-01-16 08:23:14 -0500

    feat: init createApp and server index

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import createApp from "@/lib/createApp";
+
+const app = createApp();
+
+export default app;
---
+++ src/lib/createApp.ts
@@ -0,0 +1,41 @@
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+
+import { config } from "dotenv";
+import { Hono } from "hono";
+
+config();
+
+export function createRouter() {
+  return new Hono({ strict: false });
+}
+
+export default function createApp() {
+  const app = new Hono({ strict: false });
+
+  app.notFound((context) => {
+    return context.json(
+      {
+        message: `Not found - ${context.req.path}`,
+      },
+      404,
+    );
+  });
+
+  app.onError((err, context) => {
+    const currentStatus =
+      "status" in err ? err.status : context.newResponse(null).status;
+    const statusCode =
+      currentStatus !== "OK" ? (currentStatus as ContentfulStatusCode) : 500;
+    const env = process.env.NODE_ENV;
+    return context.json(
+      {
+        message: err.message,
+
+        stack: env === "production" ? undefined : err.stack,
+      },
+      statusCode,
+    );
+  });
+
+  return app;
+}
