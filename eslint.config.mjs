3f3507126b06a9b37e5bfac7bb8ed9587c96893a
wkflanders <wflan@seas.upenn.edu>
2026-01-16 08:24:00 -0500

    build: update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: false,
+    stylistic: false,
+    ignores: ["**/migrations/*"],
+  },
+  {
+    rules: {
+      "no-console": "off",
+      "antfu/no-top-level-await": "off",
+      "node/prefer-global/process": "off",
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          internalPattern: ["^@/.*"],
+        },
+      ],
+    },
+  },
+);
