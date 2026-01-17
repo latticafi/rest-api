7cb641a730c244eab0094c431f48ce96614f602a
wkflanders <wflan@seas.upenn.edu>
2026-01-17 11:48:40 -0500

    feat: init drizzle pg schema

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
