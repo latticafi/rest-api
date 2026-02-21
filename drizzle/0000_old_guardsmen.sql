5e8da6bb2f27691abfe2b1e7cd9e90fd858edbd5
wkflanders <wflan@seas.upenn.edu>
2026-02-20 22:37:07 -0500

    chore: add db commands + migrations

---
+++ drizzle/0000_old_guardsmen.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
