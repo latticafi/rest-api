fed018afd8928781753eb382cdf823c53bab4a62
wkflanders <wflan@seas.upenn.edu>
2026-02-22 19:49:33 -0500

    feat: add auth and privy to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import configureOpenAPI from "@/lib/configureOpenAPI";
 import createApp from "@/lib/createApp";
+import { authMiddleware } from "@/middleware/auth";
 import usersRouter from "@/routes/users/users.index";
 
 const app = createApp();
+
+app.use("*", authMiddleware);
 
 app.route("/", usersRouter);
 
